f70542a5af299391c6e48e9414fdfc5c1e5017eb
Shub3am <shubhamvishwakarma0604@gmail.com>
2024-01-07 01:52:40 +0530

    Feature: Adding Choosing Video and Audio Feature Before Meeting

--- app/meet/page.tsx
+++ app/meet/page.tsx
@@ -9,28 +9,44 @@ export default function Meet() {
         "IceServers": [{ "url": "stun:freestun.net:3479" }, {"url": "stun:stun.l.google.com:19302"}],
       };
 
-
+    const [videoDevices, setVideoDevices] = useState([])
+    const [audioDevices, setAudioDevices] = useState([])
     const [connection, setConnection] = useState([])
     const [localStream, setLocal] = useState({})
     const [remoteStream, setRemote] = useState([])
     useEffect(()=> {
         (async()=> {
-            
+            let allDevices = await navigator.mediaDevices.enumerateDevices()
+            let vidDevices = []
+            let audDevices = []
+            allDevices.forEach(item => (item.kind == "videoinput") ? vidDevices.push(item): audDevices.push(item))
+            setVideoDevices(vidDevices)
+            setAudioDevices(audDevices)
+
             let local = await navigator.mediaDevices.getUserMedia({video: true, audio: true})
+            // await navigator.mediaDevices.selectAudioOutput({deviceId: "968cfffeb0873b36228f1c19bb164cdbbba90819e6b7a16947127997a4fd0972"})
+            const audioContext = new AudioContext()
+            console.log(allDevices)
             let localHost = new RTCPeerConnection(config)
             local.getTracks().forEach(track => localHost.addTrack(track, local))
-            localHost.ontrack(track=> {
-                setRemote(track.streams[0])
-            })
+            console.log(localHost.ontrack)
             setConnection(localHost)
             setLocal(local)
             
         })()
     }, [])
     if (localStream.id && connection && remoteStream.id) {
+
     return <div><h1>Others</h1><ReactPlayer url={localStream} playing={true}/><ReactPlayer url={remoteStream}/></div> }
-    else if (localStream.id && connection) {
-        return (<div><h1>Only Me</h1><ReactPlayer url={localStream} playing={true}/></div>)
+    // else if (localStream.id && connection) {
+    //     console.log(audioDevices)
+    //     return (<div><h1>Only Me</h1><ReactPlayer url={localStream} playing={true}/></div>)
+    // } 
+    else {
+        console.log(videoDevices, audioDevices)
+        const chooseVideo = videoDevices.map(item => <option value={item.deviceId}>{item.label}</option> )
+        const chooseAudio = audioDevices.map(item => <option value={item.deviceId}>{item.label}</option> )
+        return <div><select>{chooseVideo}</select><select>{chooseAudio}</select></div>
     }
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import Banner from './components/Header/Banner'
 
 export default function Home() {
   return (
     <main>
+<Banner/>
     </main>
   )
 }
